b9136521c296c0c613954e5f4545399c09af7442
ypexxx <yua.alvin@gmail.com>
2025-06-28 00:42:18 +0700

    add new file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def hello():
+    return "Hello from Railway!"
